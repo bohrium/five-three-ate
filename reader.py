559e9b51e65e035756c7aa8665e41d271adf724a
sam <samtenka@mit.edu>
2025-07-08 00:58:33 -0400

    simplify staff notation; interval-based notation

--- reader.py
+++ reader.py
@@ -12,31 +12,28 @@ import sys
 
 MOD_SYMBS = '#^vb'
 
-text = '''6 x 12
+text = '''
+6 x 12
 
-.   |           |           |           |           |           |           |
 .
 .
 .
-.
-.
-.   |-----------------------------------------------------------------------|
+.   |-----------+-----------+-----------+-----------+-----------+-----------|
 .   |                                                                       |
-.   |-----------------------------------------------------------------------|
+.   |-----------+-----------+-----------+-----------+-----------+-----------|
 .   |                                                                       |
-.   b(==long===)------------------------------------------------------------|
+.   |----------b(==moohi====)-----------+-----------+-----------+-----------|
 .   |                                                                       |
-G4  |-----------------------------------------------------------------------|
+G4  |-----------(==moomid===)-----------+-----------+-----------+-----------|
 .   |                                                                       |
-.   |-----------------------------------------------------------------------|
-.
-.       (=s)
+.   |----------b(==moolo====)-----------+-----------+-----------+-----------|
 .
 .
 .
-.   |           |           |           |           |           |           |
 where
-null
+moolo moomid M3
+moolo moohi   5
+.
 
 
 '''
@@ -107,7 +104,6 @@ CLEF_ANCHOR_VALS_BY_NAME = {'F3':-FIFTH_7, 'C4':0, 'G4':+FIFTH_7}
 def get_offset(staff):
     starts = [(i,ln.split()[0]) for i,ln in enumerate(staff)]
     starts = [(i,s) for i,s in starts if s != '.']
-    print(starts)
     assert len(starts)==1
     idx, name = starts[0]
     assert name in CLEF_ANCHOR_VALS_BY_NAME, 'clef unrecognized!'
@@ -130,25 +126,93 @@ def mod_val_53(modstr):
         -4 * counts['b']
         )
 
+INTERVALS_BY_NAME = {
+    'M2-': 8,
+    'M2' : 9,
+    'M2+':10,
+    'm3-':12,
+    'm3' :14,
+    'M3' :17,
+    'M3+':19,
+     '4' :22,
+     't' :26,
+     'T' :27,
+     '5' :31,
+    'm6-':34,
+    'm6' :36,
+    'M6' :39,
+    'M6+':41,
+    'M7-':43,
+    'M7' :44,
+    'M7+':45,
+}
+
+def interval_from_name(iname):
+    '''
+    NOTE: no signs (+/-) allowed
+    For literal numbers, precede with "="
+    '''
+    assert iname in INTERVALS_BY_NAME or (iname[0]=='=' and iname[1:].isnumeric())
+    if iname in INTERVALS_BY_NAME:
+        return INTERVALS_BY_NAME[iname]
+    else:
+        return int(iname[1:])
+
+
 def process_chunk(c):
     flanked_staff, annotations = c.split('\nwhere\n')
     flanked_staff = flanked_staff.split('\n')
-    assert len(flanked_staff) == 1+5+9+5+1
-    assert flanked_staff[0]==prefix+ruling
-    assert flanked_staff[-1]==prefix+ruling
+    assert len(flanked_staff) == 3+9+3
+    #assert flanked_staff[0]==prefix+ruling
+    #assert flanked_staff[-1]==prefix+ruling
 
-    staff = flanked_staff[1:-1]
+    all_notes = {}
+
+    staff = flanked_staff#[1:-1]
     offset_idx, offset_val = get_offset(staff)
     for i,ln in enumerate(staff):
         val_7 = offset_idx - i + offset_val
         val_53 = val_53_from_7(val_7)
-        notes = process_line(ln[len(prefix):])
-        notes = [
-            (start, end, val_53 + mod_val_53(modstr), name)
-            for
-            (start, end, name, modstr) in notes
-                ]
-        print(val_53, notes)
+        #notes = process_line(ln[len(prefix):])
+        #notes = [
+        #    (start, end, val_53 + mod_val_53(modstr), name)
+        #    for
+        #    (start, end, name, modstr) in notes
+        #        ]
+        for (start, end, name, modstr) in process_line(ln[len(prefix):]):
+            modval_53 = val_53 + mod_val_53(modstr)
+            assert name not in all_notes, "a bar's notes must have distinct names"
+            all_notes[name] = (start, end, modval_53)
+        #print(val_53, notes)
+   #$print(all_notes)
+
+    all_constraints = []
+    constraints = annotations.split('\n')
+    for ln in constraints:
+        if ln=='.': continue
+        lo, hi, iname = ln.split()
+        interval = interval_from_name(iname)
+        assert lo in all_notes
+        assert hi in all_notes
+        start = min(all_notes[nm][0] for nm in (lo,hi))
+        loval, hival = (all_notes[nm][2] for nm in (lo,hi))
+        assert loval < hival
+        sortby = (start, loval, hival)
+        all_constraints.append((sortby, lo, hi, interval))
+    all_constraints = sorted(all_constraints)
+    #print(all_constraints)
+
+    for _, lo, hi, interval in all_constraints:
+        # TODO: by precedence (before-ness or lowness)
+        slo, elo, vlo = all_notes[lo]
+        shi, ehi, vhi = all_notes[hi]
+        new_vhi = vlo + interval
+        print(all_notes[lo], all_notes[hi], interval)
+        assert abs(new_vhi - vhi) < 4, str((new_vhi, vhi))
+        assert vhi != vlo
+        assert (new_vhi<vlo and vhi<vlo) or (new_vhi>vlo and vhi>vlo)
+        all_notes[hi] = (shi, ehi, new_vhi)
+    #print(all_notes)
 
 for c in body:
     process_chunk(c)
